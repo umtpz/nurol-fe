632d1e81606679a9b537d8239884be691f714c39
Umut Topuz <umutopuz@gmail.com>
2024-01-02 16:51:52 +0300

    title fix

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -17,7 +17,7 @@ import { CoreConfig } from '@core/types';
 export const coreConfig: CoreConfig = {
   app: {
     appName     : 'Vuexy',                                        // App Name
-    appTitle    : 'Vuexy - Angular 14+ Bootstrap Admin Template', // App Title
+    appTitle    : 'NurolBank', // App Title
     appLogoImage: 'assets/images/logo/logo.svg',                  // App Logo
     appLanguage : 'en',                                           // App Default Language (en, fr, de, pt etc..)
   },
